8e2ad0cfec0202796888601aa0e6b948b89d90d5
Amitkumar Vishwakarma <1.amitkumarv@gmail.com>
2020-05-26 16:34:16 +0530

    changes in js by fish

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ console.log("Hello this is the git tutorial");
 setTimeout(function something() {
     console.log(chicken);
 }, 1000);
+
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+// expected output: true
